1aa4bd4e4d510834ec4185c9ab5c787cf9ec90c7
yrchalla <210050171@iitb.ac.in>
2022-10-22 19:13:37 +0530

    added detailed comments and running instructions

--- client.py
+++ client.py
@@ -1,14 +1,15 @@
 import socket
-
 s = socket.socket()
+
 port = 1234
-
-
 s.connect(('localhost', port))
 mess = ""
 
+# receive a message from the server (written by the other client)
 incomMess = s.recv(1024)
-while incomMess!="quit":
+
+# funnily, if the other client types quit, this client's connection will close
+while incomMess != "quit":
     print(incomMess.decode())
     a = input()
     s.send(a.encode())
--- server.py
+++ server.py
@@ -1,45 +1,51 @@
 # Most imortant note:
-# DO CLOSE THE SOCKETS WHENEVER YOU OPENS THEM ! ELSE YOU WOULD RUN OUT OF THEM FOR SOME TIME!
+# DO CLOSE THE SOCKETS WHENEVER YOU OPEN THEM TO PREVENT RESOURCE LEAK!
 
- 
+
 from email import message
 import socket
 s = socket.socket()
 
 port = 1234
-s.bind(('', port)) # 
+# this socket instance will bind to a client with any ip address, but the clients port must be the same as 'port'
+s.bind(('', port))
 
-s.listen(10) # maximum number of allowed connections
-print(f"Listening on port{port}......")
+s.listen(10)  # maximum number of allowed connections
+print(f"Listening on port {port}......")
 
-
+# this list stores clients in the chronological order of their connection to the server
 clients = []
 
+# this loop makes the program wait till the socket instance s connects to two clients
 while True:
     c, adrr = s.accept()
     clients.append(c)
-    if(len(clients) == 2):
+    if (len(clients) == 2):
         break
 
+# A is the client that connects first and B is the one who connects next
+# the messages sent by A and B in chronological order
 messageA = []
 messageB = []
 
-num = 0
-while len(clients)!=0:
-    if(num == 0):
-        if(len(messageB) == 0):
-            clients[num].send("==> No message yet <==".encode())
+
+# following loop works properly only when the two clients send messages alternately. If either one sends a message out of his/her turn, the message will be stored in the list and sent eventually.
+num = 0  # represents which client's turn it is to talk. 0 means A should be talking
+while len(clients) != 0:
+    if (num == 0):
+        if (len(messageB) == 0):
+            clients[0].send("==> No message yet <==".encode())
         else:
-            clients[num].send(messageB[-1].encode())
-        messageA.append(f"B:{clients[num].recv(1024).decode('utf-8')}")
-        num = (num+1)%2
-    elif(num == 1):
-        clients[num].send(messageA[-1].encode())
-        messageB.append(f"A:{clients[num].recv(1024).decode('utf-8')}")
-        num = (num+1)%2
+            clients[0].send(messageB[-1].encode())
+        # recv is a blocking method. Therefore any messages sent by B in this timeframe will be stored in the list but not printed on A's terminal
+        messageA.append(f"A:{clients[0].recv(1024).decode('utf-8')}")
+        num = 1
+    elif (num == 1):
+        clients[1].send(messageA[-1].encode())
+        # recv is a blocking method. Therefore any messages sent by A in this timeframe will be stored in the list but not printed on B's terminal
+        messageB.append(f"B:{clients[1].recv(1024).decode('utf-8')}")
+        num = 0
 
 
-
-# HAVE TO FIX THIS (server sockt must close.)
+# HAVE TO FIX THIS (server socket must close.)
 s.close()
-
